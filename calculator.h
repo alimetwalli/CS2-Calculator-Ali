0bcea6813ac0dec3e623de0b7c9b5c4ceb723595
Ali Metwalli <ametwalli@aucegypt.edu>
2025-04-08 00:21:26 +0200

    added ; to calculator.h and fixed capitilization in a declaration

--- calculator.h
+++ calculator.h
@@ -1,16 +1,16 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
-Class Calculator {
+class Calculator {
 public:
 
-    double add(double a,double b)
-    double subtract(double a,double b)
-    double multiply(double a,double b)
-    double divide(double a,double b)
-    int factorial(int n)
-    int GCD(int a, int b)
-    int LCM(int a, int b)
-    int randomNumber(int min, int max)
+    double add(double a,double b);
+    double subtract(double a,double b);
+    double multiply(double a,double b);
+    double divide(double a,double b);
+    int factorial(int n);
+    int GCD(int a, int b);
+    int LCM(int a, int b);
+    int randomNumber(int min, int max);
 };
 #endif
