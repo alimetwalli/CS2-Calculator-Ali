ecd1744c8f778358d17d2de396bd27d845f28717
Ali Metwalli <ametwalli@aucegypt.edu>
2025-04-08 00:31:40 +0200

    fixed spelling and syntax errors

--- test.cpp
+++ test.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main (){
-    Calcylatir calc;
-    cout<<"8+7 = "<<add(8,7)<<endl;
-    cout<<"19-7 = "<<subtract(19,7)<<endl;
-    cout<<"4*6 = "<<multiply(4,6)<<endl;
-    cout<<"8/2 = "<<divide(8,2)<<endl;
-    cout<<"8/0 = "<<divide(8,0)<<endl;
-    cout<<"5! = "<<factorial(5)<<endl;
-    cout<<"-5! = "<<factorial(-5)<<endl;
-    cout<<"GCD(12,8) = "<<GCD(12,8)<<endl;
-    cout<<"LCM(5,3) = "<<LCM(5,3)<<endl;
+    Calculator calc;
+    cout<<"8+7 = "<<calc.add(8,7)<<endl;
+    cout<<"19-7 = "<<calc.subtract(19,7)<<endl;
+    cout<<"4*6 = "<<calc.multiply(4,6)<<endl;
+    cout<<"8/2 = "<<calc.divide(8,2)<<endl;
+    cout<<"8/0 = "<<calc.divide(8,0)<<endl;
+    cout<<"5! = "<<calc.factorial(5)<<endl;
+    cout<<"-5! = "<<calc.factorial(-5)<<endl;
+    cout<<"GCD(12,8) = "<<calc.GCD(12,8)<<endl;
+    cout<<"LCM(5,3) = "<<calc.LCM(5,3)<<endl;
     cout<<"Random number between 1 and 10: "<<randomNumber(1,10)<<endl;
 
 
