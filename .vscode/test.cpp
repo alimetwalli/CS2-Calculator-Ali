6d1ddb6377016169a3340bca3dfd40bd10b9f2fa
Ali Metwalli <ametwalli@aucegypt.edu>
2025-04-07 23:14:21 +0200

    calculator.cpp and test.cpp

---
+++ .vscode/test.cpp
@@ -0,0 +1,22 @@
+#include "calculator.h"
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<<"8+7 = "<<add(8,7)<<endl;
+    cout<<"19-7 = "<<subtract(19,7)<<endl;
+    cout<<"4*6 = "<<multiply(4,6)<<endl;
+    cout<<"8/2 = "<<divide(8,2)<<endl;
+    cout<<"8/0 = "<<divide(8,0)<<endl;
+    cout<<"5! = "<<factorial(5)<<endl;
+    cout<<"-5! = "<<factorial(-5)<<endl;
+    cout<<"GCD(12,8) = "<<GCD(12,8)<<endl;
+
+
+
+
+
+
+
+    return 0;
+}
